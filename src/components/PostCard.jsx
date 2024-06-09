5827f03e82fd41cf241f320ad3a6bbc77abed206
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2024-06-09 16:24:16 -0300

    update PostCard
    formatando post card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,12 +1,28 @@
 //Resumo de um tópico
+import { format } from 'date-fns';
 
 export default function PostCard(props){
 
     const {topico} = props;
+    const dataFormatada = format(newDate(topico.dataPublicacao), 'dd/MM/yyyy');
 
     return(
-        <div>
+        <div style = {styles.container}>
+           <span>
             <h1>{topico.titulo}</h1>
+            <p>Publicado: {dataFormatada} </p>
+            <p> Palavras-chave: {topico.palavrasChave}</p>
+           </span> 
         </div>
     )
 }
+
+const styles = {
+    container: {
+      display: "flex",
+      padding: 5,
+      margin: 10,
+      backgroundColor: "#8ecae6",
+      borderRadius: 2,
+    },
+  };
